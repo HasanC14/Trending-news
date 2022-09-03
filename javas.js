4fcc1fd6961932cb58fdcd35f5457e3202259d39
HasanC14 <chowdhuryh999@gmail.com>
2022-09-04 03:17:14 +0600

    Might be final

--- javas.js
+++ javas.js
@@ -1,8 +1,14 @@
-// Loading Ctagories
+// Loading Catagoriesa
 const LoadCtagories = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
-        .then(res => res.json())
-        .then(data => NavDisplay(data.data))
+    fetch('https://openapi.programming-hero.com/api/news/categories').then(function (response) {
+        if (response.status == 200) {
+            return response.json()
+        }
+    }).then((data) => {
+        NavDisplay(data.data);
+    }).catch(() => {
+        console.log("Error");
+    });
 }
 const NavDisplay = ctagories => {
     ctagories.news_category.forEach(ctagory => {
@@ -16,9 +22,6 @@ const NavDisplay = ctagories => {
 }
 LoadCtagories();
 
-
-
-
 //Load News
 const more = (id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
@@ -28,12 +31,23 @@ const more = (id) => {
     loader(true);
 }
 const CardDisplay = ctagories => {
+    // Sorting Algorithm
+    ctagories.sort((a, b) => {
+        return b.total_view - a.total_view;
+    });
+    //Total Data Found Message
     NewsCount(ctagories.length);
-    const card = document.getElementById('card');
+    //No Data Found Message
+    if (ctagories.length === 0) {
+        document.getElementById('Warning').classList.remove('d-none');
+    }
+    else document.getElementById('Warning').classList.add('d-none');
+
+    let card = document.getElementById('card');
     card.innerHTML = '';
+    //Creatig the News Card Section
     ctagories.forEach(ctagory => {
-        NewsCount(ctagories.length);
-        const CardBody = document.createElement('div');
+        let CardBody = document.createElement('div');
         card.appendChild(CardBody);
         CardBody.classList.add('row');
         CardBody.innerHTML = `
@@ -49,8 +63,8 @@ const CardDisplay = ctagories => {
                             <h5 class="card-title">${ctagory.title}</h5>
                             <p class="card-text">${ctagory.details.slice(0, 200)}${'...'}</p>
                             <div class="d-flex justify-content-around mt-5">
-                        <div><img src="${ctagory.author.img}" alt="..." class="avatar me-2"><span>${ctagory.author.name}</span></div>
-                        <i class="fa-regular fa-eye mt-3"> ${ctagory.total_view}</i>
+                        <div><img src="${ctagory.author.img}" alt="..." class="avatar me-2"><span>${ctagory.author.name ? ctagory.author.name : "No data Found"}</span></div>
+                        <i class="fa-regular fa-eye mt-3"> <span class="ms-1">${ctagory.total_view ? ctagory.total_view : "No data"}</span> </i>
                         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="modal('${ctagory._id}')">Read More</button>
                     </div>
                         </div>
@@ -100,110 +114,4 @@ const loader = (isloading) => {
     else {
         loader.classList.add('d-none')
     }
-}
-
-//Default Sorting
-const DefaultSort = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
-        .then(res => res.json())
-        .then(data => DefaultCatagories(data.data))
-}
-const DefaultCatagories = ctagories => {
-    ctagories.news_category.forEach(ctagory => {
-        DefaultctagoriesID(ctagory.category_id);
-    });
-}
-const DefaultctagoriesID = (id) => {
-    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => DefaultSortDisplay(data.data))
-}
-const DefaultSortDisplay = ctagories => {
-    NewsCount(ctagories.length);
-    const card = document.getElementById('card');
-    card.innerHTML = '';
-    ctagories.forEach(ctagory => {
-        NewsCount(ctagories.length);
-        const CardBody = document.createElement('div');
-        card.appendChild(CardBody);
-        CardBody.classList.add('row');
-        CardBody.innerHTML = `
-        <div class="col-12 mb-3 d-flex justify-content-center align-items-center">
-            <div class="card w-75 shadow-lg p-3 mb-3 bg-white rounded  ">
-                <div class="row">
-                    <div class="col-lg-6 col-md-12 col-sm-12 d-flex justify-content-center align-items-center">
-                            <img src="${ctagory.thumbnail_url}"
-                            class="card-img-top img-fluid w-75">
-                    </div>
-                    <div class="col-lg-6 col-md-12 col-sm-12 d-flex justify-content-center align-items-center">
-                    <div class="card-body">
-                            <h5 class="card-title">${ctagory.title}</h5>
-                            <p class="card-text">${ctagory.details.slice(0, 200)}${'...'}</p>
-                            <div class="d-flex justify-content-around mt-5">
-                        <div><img src="${ctagory.author.img}" alt="..." class="avatar me-2"><span>${ctagory.author.name}</span></div>
-                        <i class="fa-regular fa-eye mt-3"> ${ctagory.total_view}</i>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="modal('${ctagory.rating.number}','${ctagory.rating.badge}')">See Ratings</button>
-                    </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `;
-        loader(false);
-    });
-}
-
-//Sorting By Top View
-const Sort = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
-        .then(res => res.json())
-        .then(data => catagories(data.data))
-}
-const catagories = ctagories => {
-    ctagories.news_category.forEach(ctagory => {
-        ctagoriesID(ctagory.category_id);
-    });
-}
-const ctagoriesID = (id) => {
-    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => SortDisplay(data.data))
-}
-const SortDisplay = ctagories => {
-    NewsCount(ctagories.length);
-    const card = document.getElementById('card');
-    card.innerHTML = '';
-    ctagories.forEach(ctagory => {
-        NewsCount(ctagories.length);
-        const CardBody = document.createElement('div');
-        card.appendChild(CardBody);
-        CardBody.classList.add('row');
-        CardBody.innerHTML = `
-        <div class="col-12 mb-3 d-flex justify-content-center align-items-center">
-            <div class="card w-75 shadow-lg p-3 mb-3 bg-white rounded  ">
-                <div class="row">
-                    <div class="col-lg-6 col-md-12 col-sm-12 d-flex justify-content-center align-items-center">
-                            <img src="${ctagory.thumbnail_url}"
-                            class="card-img-top img-fluid w-75">
-                    </div>
-                    <div class="col-lg-6 col-md-12 col-sm-12 d-flex justify-content-center align-items-center">
-                    <div class="card-body">
-                            <h5 class="card-title">${ctagory.title}</h5>
-                            <p class="card-text">${ctagory.details.slice(0, 200)}${'...'}</p>
-                            <div class="d-flex justify-content-around mt-5">
-                        <div><img src="${ctagory.author.img}" alt="..." class="avatar me-2"><span>${ctagory.author.name}</span></div>
-                        <i class="fa-regular fa-eye mt-3"> ${ctagory.total_view}</i>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="modal('${ctagory.rating.number}','${ctagory.rating.badge}')">See Ratings</button>
-                    </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `;
-        loader(false);
-    });
 }
